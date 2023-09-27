946c48631cd49cef56846e2767c7589a1a983073
littyann <littyannjose14@gmail.com>
2023-09-27 14:37:24 +0530

    secondCommit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Banner from './components/Banner';
+import Navbar from './components/Navbar';
+import Row from './components/Row';
+import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Netflix</h1>
+    <div>
+      <Navbar/>
+      <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+     <Row isPoster={true} title="Netfix Orginals" fetchUrl={requests.fetchNetflixOriginals} />
+     <Row title="Trending In India" fetchUrl={requests.fetchTrending} />
+     <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRated} />
+     <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+     <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+     <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+     <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+     <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    const [show,setShow] = useState(false)
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY>600){
+                setShow(true)
+            }
+            else{
+                setShow(false)
+            }
+        })
+    })
+  return (
+    <div className={`${show && 'nav-black'} nav`}>
+        <img width={'150px'} src="https://farm6.staticflickr.com/5821/20639706793_8c038faa4a_o.png" alt="netflix logo" />
+
+    </div>
+  )
+}
+
+export default Navbar
